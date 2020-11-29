e0962177d21f3b9e664a23d400bb40e4409acf82
zhongMou-lilSister <3180104945@zju.edu.cn>
2020-11-29 12:40:32 +0800

    12345

--- InstructionROM.v
+++ InstructionROM.v
@@ -22,21 +22,21 @@ module InstructionROM(addr,dout);
 	reg [31 : 0] dout;
 	always @(*)
 		case (addr)
-			6'd0:   dout<=32'h0000_3f37 ;//           lui X30,0x3000
-			6'd1:   dout<=32'h0200_0fE7 ;//           jalr X31 later(X0)
-			6'd2:   dout<=32'h01c0_2623 ;// earlier: sw  X28, 0C(X0) 
-			6'd3:   dout<=32'h0043_2e83 ;//           lw  X29, 4(X6) 
-			6'd4:   dout<=32'h002e_9293 ;//           sll  X5, X29, 2 
-			6'd5:   dout<=32'h0043_2e03 ;//           lw   X28, 4(X6)
-			6'd6:   dout<=32'h0073_3e33 ;//		     sltu X28,X6,X7 
-			6'd7:   dout<=32'h0000_0f6f ;//	done:   jal X31,done
-			6'd8:   dout<=32'h0000_1c63 ;//	later:	bne X0, X0, end  	
-			6'd9:   dout<=32'h042f_0293;//	        addi X5, X30, 42
-			6'd10:  dout<=32'h01f0_0333 ;//          add  X6, X0, X31 
-			6'd11:  dout<=32'h4062_83b3 ;//           sub X7, X5, X6
-			6'd12:  dout<=32'h0053_ee33 ;//       	or	 X28, X7, X5 
-			6'd13:  dout<=32'hfc00_0ae3 ;//          beq X0, X0, earlier
-			6'd14:  dout<=32'h00000000 ;// end:  	nop
-			default:dout<=32'h00000000 ;//nop
+			6'd0:   dout<=32'h0000_3f37 ;//00         lui  X30, 0x3000
+			6'd1:   dout<=32'h0200_0fE7 ;//04         jalr X31 later(X0)
+			6'd2:   dout<=32'h01c0_2623 ;//08 earlier:sw   X28, 0C(X0) 
+			6'd3:   dout<=32'h0043_2e83 ;//0c         lw   X29, 4(X6) 
+			6'd4:   dout<=32'h002e_9293 ;//10         sll  X5,  X29, 2 
+			6'd5:   dout<=32'h0043_2e03 ;//14         lw   X28, 4(X6)
+			6'd6:   dout<=32'h0073_3e33 ;//18	      sltu X28, X6,X7 
+			6'd7:   dout<=32'h0000_0f6f ;//1c done:   jal  X31, done
+			6'd8:   dout<=32'h0000_1c63 ;//20 later:  bne  X0,  X0, end  	
+			6'd9:   dout<=32'h042f_0293; //24         addi X5,  X30, 42
+			6'd10:  dout<=32'h01f0_0333 ;//28         add  X6,  X0, X31 
+			6'd11:  dout<=32'h4062_83b3 ;//2c         sub  X7,  X5, X6
+			6'd12:  dout<=32'h0053_ee33 ;//30     	  or   X28, X7, X5 
+			6'd13:  dout<=32'hfc00_0ae3 ;//34         beq  X0,  X0, earlier
+			6'd14:  dout<=32'h00000000 ; //38 end:    nop
+			default:dout<=32'h00000000 ; //nop
 		endcase	
 endmodule
--- Registers.v
+++ Registers.v
@@ -12,8 +12,8 @@ module Registers(rs1Data, rs2Data, clk, rs1Addr, rs2Addr, WriteAddr,
     wire rs1Sel;
     wire rs2Sel;
 
-    wire temp1;
-    wire temp2;
+    wire [31:0] temp1;
+    wire [31:0] temp2;
 
     assign rs1Sel = RegWrite && (WriteAddr != 0) && (WriteAddr == rs1Addr);
     assign rs2Sel = RegWrite && (WriteAddr != 0) && (WriteAddr == rs2Addr);
@@ -21,7 +21,7 @@ module Registers(rs1Data, rs2Data, clk, rs1Addr, rs2Addr, WriteAddr,
     RBWRegisters insider(.ReadData1(temp1),.ReadData2(temp2),.clk(clk),.ReadRegisters1(rs1Addr),.ReadRegisters2(rs2Addr), 
     .WriteRegister(WriteAddr),.RegWrite(RegWrite),.WriteData(WriteData));
 
-    assign rs1Data = (rs1Sel)?temp1:WriteData;
-    assign rs2Data = (rs2Sel)?temp2:WriteData;
+    assign rs1Data = (rs1Sel)?WriteData:temp1;
+    assign rs2Data = (rs2Sel)?WriteData:temp2;
 
 endmodule
